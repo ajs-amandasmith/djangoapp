3e64cdfbd880fd461941c4914a486a3b55e80cae
Amanda Smith <ajs.amandasmith@gmail.com>
2024-07-14 15:57:13 -0500

    Debugging practice

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-  path("", views.home, name="home")
+  path("", views.home, name="home"),
+  # path - defines a route "hello/" that accepts a variable string called 'name'
+  # the string is passed to the 'views.hello_there' function specified in the second argument to 'path'
+  # URL routes are case-sensitive, if you want /hello/<name> and /Hello/<name> - define paths for each
+  path("hello/<name>", views.hello_there, name="hello_there"),
 ]
