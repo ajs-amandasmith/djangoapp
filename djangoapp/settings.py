7b2263f7801d7651d666d3766076066c0e8ba6e7
Amanda Smith <ajs.amandasmith@gmail.com>
2024-07-14 22:00:12 -0500

    Databases

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jdl^+wtv_r)u4ne=a3d+)9_py!ifth+*^9ngb(bt@+=2q7$p4)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- hello/urls.py
+++ hello/urls.py
@@ -1,15 +1,25 @@
 from django.urls import path
 from hello import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from hello.models import LogMessage
+
+home_list_view = views.HomeListView.as_view(
+  queryset=LogMessage.objects.order_by("-log_date")[:5], # Limits the results to the five most recent
+  context_object_name="message_list",
+  template_name="hello/home.html",
+)
 
 urlpatterns = [
-  path("", views.home, name="home"),
+  path("", home_list_view, name="home"),
   path("about/", views.about, name="about"),
   path("contact", views.contact, name="contact"),
+  path("log/", views.log_message, name="log"),
   # path - defines a route "hello/" that accepts a variable string called 'name'
   # the string is passed to the 'views.hello_there' function specified in the second argument to 'path'
   # URL routes are case-sensitive, if you want /hello/<name> and /Hello/<name> - define paths for each
   path("hello/<name>", views.hello_there, name="hello_there"),
 ]
+
+
 
 urlpatterns += staticfiles_urlpatterns()
